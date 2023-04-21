9e3cf400401ecd2013d9ce6bb718f77117070635
Sidx-sys <sid.iiith@outlook.com>
2023-04-21 05:44:14 +0530

    Clean final update

---
+++ spotify-api/get_token.sh
@@ -0,0 +1,3 @@
+curl -X POST "https://accounts.spotify.com/api/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=886f020eb8dc4a34976575cc87cb47fa&client_secret=f488c7e2bb674e168b0f752066cdce3d"
